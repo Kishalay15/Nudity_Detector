afea0c26e41a26ef330234906c9793e1d2e6fb52
Kishalay15 <kishalaylahiri@gmail.com>
2025-03-30 13:31:31 +0530

    doing the final test on validation dataset since train dataset has a bias

--- src/preprocess.py
+++ src/preprocess.py
@@ -75,6 +75,6 @@ def create_loaders(batch_size=32, num_workers=2):
     # Dataloaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     return {"train": train_loader, "val": val_loader, "test": test_loader}
